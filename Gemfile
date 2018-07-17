e05452a131c3315bc5de719e226624140919c5f7
Jeffrey Wong <jvwong@outlook.com>
2018-07-17 13:04:25 -0400

    Sorry can't ignore subtree folder I guess

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'jekyll-watch'
-gem 'kramdown',  '~>1.15.0'
-gem 'rouge', '~>2.2.1'
-gem 'jekyll_github_sample'
--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var guide = require('./guide/guide.js');
-var ga = require('./vendor/ga.js');
-
-$(document).ready(function(){
-  ga.initModule();
-  guide.initModule();
-});
